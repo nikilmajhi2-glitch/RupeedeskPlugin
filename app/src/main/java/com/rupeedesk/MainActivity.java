f5b2ed80cb663b02bc459a072565150b7c0fd23e
nikilmajhi2-glitch <nikhilmajhi2@gmail.com>
2025-11-07 22:09:46 +0530

    update MainActivity.java

--- app/src/main/java/com/rupeedesk/MainActivity.java
+++ app/src/main/java/com/rupeedesk/MainActivity.java
@@ -2,6 +2,7 @@ package com.rupeedesk;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
